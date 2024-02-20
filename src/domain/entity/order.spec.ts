00ac79e2e99fdac6a974b4e18f082e2f2fd33862
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2024-02-19 23:48:55 -0300

    fixes throw in test cases

--- src/domain/entity/order.spec.ts
+++ src/domain/entity/order.spec.ts
@@ -2,19 +2,19 @@ import Order from "./order";
 import OrderItem from "./order_item";
 
 describe("Order unit tests", () => {
-  it("should trow error when id is empty", () => {
+  it("should throw error when id is empty", () => {
     expect(() => {
       new Order("", "123", []);
     }).toThrow("Id is required");
   });
 
-  it("should trow error when customerId is empty", () => {
+  it("should throw error when customerId is empty", () => {
     expect(() => {
       new Order("123", "", []);
     }).toThrow("CustomerId is required");
   });
 
-  it("should trow error when items is empty", () => {
+  it("should throw error when items is empty", () => {
     expect(() => {
       new Order("123", "123", []);
     }).toThrow("Items list must have at least one item");
@@ -33,5 +33,5 @@ describe("Order unit tests", () => {
     const item2 = new OrderItem("123", "123", 2, 20);
     const order = new Order("123", "123", [item1, item2]);
     expect(order.total()).toBe(60);
-  })
+  });
 });
