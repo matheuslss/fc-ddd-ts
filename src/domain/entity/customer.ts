3dae9bda09ae43c23b2b9eac2b90d2442d19fd0a
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2024-02-19 22:15:26 -0300

    add validate method to customer entity

--- src/domain/entity/customer.ts
+++ src/domain/entity/customer.ts
@@ -10,5 +10,16 @@ export default class Customer {
     constructor(id: string, name: string) {
         this._id = id
         this._name = name
+        this.validate()
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("Id is required")
+        }
     }
 }
