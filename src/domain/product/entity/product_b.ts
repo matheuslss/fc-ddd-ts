990e09ff0432d3d869e678c458de78f4a8b5f025
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2024-02-22 18:08:23 -0300

    add product factory

--- src/domain/product/entity/product_b.ts
+++ src/domain/product/entity/product_b.ts
@@ -1,6 +1,6 @@
 import ProductInterface from "./interface";
 
-export default class Product implements ProductInterface {
+export default class ProductB implements ProductInterface {
   private _id: string;
   private _name: string;
   private _price: number;
@@ -21,30 +21,29 @@ export default class Product implements ProductInterface {
   }
 
   get price(): number {
-    return this._price;
+    return this._price * 2;
   }
 
-  validate() {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
-    }
-
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
-    }
-
-    if (this._price <= 0) {
-      throw new Error("Price must be greater than zero");
-    }
-  }
-
-  changeName(name: string) {
+  changeName(name: string): void {
     this._name = name;
     this.validate();
   }
 
-  changePrice(price: number) {
+  changePrice(price: number): void {
     this._price = price;
     this.validate();
+  }
+
+  validate(): boolean {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._price < 0) {
+      throw new Error("Price must be greater than zero");
+    }
+    return true;
   }
 }
